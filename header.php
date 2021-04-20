4107c704803b05ee467a9950e55bd55b35922c34
meghakansal12 <45147323+meghakansal12@users.noreply.github.com>
2021-04-20 06:44:40 -0700

    header updated

--- header.php
+++ header.php
@@ -44,5 +44,4 @@
   <script src="assets/plugins/jquery-ui/jquery-ui.min.js"></script>
  <!-- summernote -->
   <link rel="stylesheet" href="assets/plugins/summernote/summernote-bs4.min.css">
-  
 </head>
